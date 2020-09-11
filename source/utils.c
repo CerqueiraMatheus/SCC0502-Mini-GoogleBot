eec3066e1ed145bd979be57d55cbfafa7b69e402
Matheus Cerqueira <matheus.crqra@gmail.com>
2020-09-11 17:17:16 -0300

    Adicionei um arquivo de funçoes uteis. Atualmente, apenas com uma readLine.

---
+++ source/utils.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define INPUT_BUFFER 2048
+
+// Retorna uma linha a partir de um arquivo.
+char *getLineFromFile(FILE *inputFile) {
+    char *line = NULL;
+    int pos = 0;
+
+    do {
+        if (pos % INPUT_BUFFER == 0) {
+            line = (char *)realloc(line,
+                                   (pos / INPUT_BUFFER + 1) * INPUT_BUFFER);
+        }
+        char aux = (char)fgetc(inputFile);
+        if (aux != '\r') line[pos++] = aux;
+    } while (line[pos - 1] != '\n' && !feof(inputFile));
+
+    line[pos - 1] = '\0';
+
+    return line;
+}
